36c771ea2e29f9324ba1ac28fc4fae514b1c51d8
thiakovidis <thiakovidis@agroapps.gr>
2022-09-13 13:05:59 +0300

    Store new user using in firestore authentication service after filling the register form

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -107,6 +107,8 @@
 </template>
 
 <script>
+import firebase from "@/includes/firebase";
+
 export default {
   name: "RegisterForm",
   data() {
@@ -131,15 +133,30 @@ export default {
     };
   },
   methods: {
-    register(values) {
+    async register(values) {
       this.reg_show_alert = true;
       this.reg_in_submission = true;
       this.reg_alert_variant = "bg-blue-500";
       this.reg_alert_message = "Please wait! Your account is being created.";
 
+      let userCred = null;
+
+      try {
+        userCred = await firebase
+          .auth()
+          .createUserWithEmailAndPassword(values.email, values.password);
+      } catch (error) {
+        console.log(error);
+        this.reg_in_submission = false;
+        this.reg_alert_variant = "bg-red-500";
+        this.reg_alert_message =
+          "An unexpected error occured. Please try again later.";
+        return;
+      }
+
       this.reg_alert_variant = "bg-green-500";
       this.reg_alert_message = "Success! Your account has been created.";
-      console.log(values);
+      console.log(userCred);
     },
   },
 };
