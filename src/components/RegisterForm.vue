7f4eb31ed6141e6044d3ac2d80830e121863a9d7
thiakovidis <thiakovidis@agroapps.gr>
2022-09-13 14:20:49 +0300

    Refactor register functionality using pinia actions

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -107,8 +107,7 @@
 </template>
 
 <script>
-import { auth, usersCollection } from "@/includes/firebase";
-import { mapWritableState } from "pinia";
+import { mapActions } from "pinia";
 import useUserStore from "@/stores/user";
 
 export default {
@@ -134,23 +133,16 @@ export default {
       reg_alert_message: "Please wait! Your account is being created.",
     };
   },
-  computed: {
-    ...mapWritableState(useUserStore, ["userLoggedIn"]),
-  },
   methods: {
+    ...mapActions(useUserStore, { createUser: "register" }),
     async register(values) {
       this.reg_show_alert = true;
       this.reg_in_submission = true;
       this.reg_alert_variant = "bg-blue-500";
       this.reg_alert_message = "Please wait! Your account is being created.";
 
-      let userCred = null;
-
       try {
-        userCred = await auth.createUserWithEmailAndPassword(
-          values.email,
-          values.password
-        );
+        await this.createUser(values);
       } catch (error) {
         console.log(error);
         this.reg_in_submission = false;
@@ -159,28 +151,9 @@ export default {
           "An unexpected error occured. Please try again later.";
         return;
       }
-
-      try {
-        await usersCollection.add({
-          name: values.name,
-          email: values.email,
-          age: values.age,
-          country: values.country,
-        });
-      } catch (error) {
-        console.log(error);
-        this.reg_in_submission = false;
-        this.reg_alert_variant = "bg-red-500";
-        this.reg_alert_message =
-          "An unexpected error occured. Please try again later.";
-        return;
-      }
-
-      this.userLoggedIn = true;
 
       this.reg_alert_variant = "bg-green-500";
       this.reg_alert_message = "Success! Your account has been created.";
-      console.log(userCred);
     },
   },
 };
