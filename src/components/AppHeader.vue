ed85e65419b51549911d3b1e3b81d1a199730989
thiakovidis <thiakovidis@agroapps.gr>
2022-09-14 09:56:26 +0300

    Redirect user from manage page after logging out

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -31,10 +31,7 @@
               >
             </li>
             <li>
-              <a
-                class="px-2 text-white"
-                href="#"
-                @click.prevent="userStore.signOut"
+              <a class="px-2 text-white" href="#" @click.prevent="signOut"
                 >Logout</a
               >
             </li>
@@ -57,6 +54,12 @@ export default {
   methods: {
     toggleAuthModal() {
       this.modalStore.isOpen = !this.modalStore.isOpen;
+    },
+    signOut() {
+      this.userStore.signOut();
+      if (this.$route.name === "Manage") {
+        this.$router.push({ name: "Home" });
+      }
     },
   },
 };
