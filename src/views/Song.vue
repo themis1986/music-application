4c688253dea2f41e77ad1e99b2c2a7fafcf3ec7b
thiakovidis <thiakovidis@agroapps.gr>
2022-09-15 16:20:50 +0300

    Update comment counter

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -25,7 +25,7 @@
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
         <!-- Comment Count -->
-        <span class="card-title">Comments (15)</span>
+        <span class="card-title">Comments ({{ song.commentCount }})</span>
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
@@ -162,6 +162,11 @@ export default {
       };
 
       await commentsCollection.add(comment);
+
+      this.song.commentCount += 1;
+      await songsCollection.doc(this.$route.params.id).update({
+        commentCount: this.song.commentCount,
+      });
 
       this.getComments();
 
