33fd4c52dda8259745725a2241554f41720240d1
thiakovidis <thiakovidis@agroapps.gr>
2022-09-17 19:21:58 +0300

    add translation fynctionality

--- src/main.js
+++ src/main.js
@@ -5,11 +5,12 @@ import App from "./App.vue";
 import router from "./router";
 import VeeValidatePlugin from "./includes/validation";
 import { auth } from "./includes/firebase";
+import i18n from "./includes/i18n";
+
+import Icon from "./directives/icon";
 
 import "./assets/base.css";
 import "./assets/main.css";
-
-import Icon from "./directives/icon";
 
 let app;
 
@@ -20,6 +21,7 @@ auth.onAuthStateChanged(() => {
     app.use(createPinia());
     app.use(router);
     app.use(VeeValidatePlugin);
+    app.use(i18n);
     app.directive("icon", Icon);
 
     app.mount("#app");
