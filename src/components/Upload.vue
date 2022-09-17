3e005be013eba6e557b5b503c006ca981cb676eb
thiakovidis <thiakovidis@agroapps.gr>
2022-09-17 22:01:49 +0300

    Handle offline uploads

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -64,6 +64,18 @@ export default {
           return;
         }
 
+        if (!navigator.onLine) {
+          this.uploads.push({
+            task: {},
+            currentProgress: 100,
+            name: file.name,
+            variant: "bg-red-400",
+            icon: "fas fa-times",
+            textClass: "text-red-400",
+          });
+          return;
+        }
+
         const storageRef = storage.ref(); // music-application-d7c4c.appspot.com
         const songsRef = storageRef.child(`songs/${file.name}`); // music-application-d7c4c.appspot.com/songs/example.mp3
         const task = songsRef.put(file);
