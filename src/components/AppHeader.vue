b448385fe958006a6cc64d8b1d7306634cbc27e8
thiakovidis <thiakovidis@agroapps.gr>
2022-09-17 20:06:27 +0300

    Add more localizations

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -37,6 +37,13 @@
             </li>
           </template>
         </ul>
+        <ul class="ml-auto">
+          <li>
+            <a href="#" class="px-2 text-white" @click.prevent="changeLocale">{{
+              currentLocale
+            }}</a>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
@@ -50,6 +57,11 @@ export default {
   name: "AppHeader",
   computed: {
     ...mapStores(useModalStore, useUserStore),
+    currentLocale() {
+      return this.$i18n.locale === "fr"
+        ? "Change to English"
+        : "Change to French";
+    },
   },
   methods: {
     toggleAuthModal() {
@@ -61,6 +73,9 @@ export default {
       if (this.$route.meta.requiresAuth) {
         this.$router.push({ name: "Home" });
       }
+    },
+    changeLocale() {
+      this.$i18n.locale = this.$i18n.locale === "fr" ? "en" : "fr";
     },
   },
 };
